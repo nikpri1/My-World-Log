7a7399b2609d394490720a7ad5f086628c7b326a
nikpri1 <nik_prime@hotmail.com>
2024-10-20 22:19:37 +0200

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
   return (
     <footer className={styles.footer}>
       <p className={styles.copyright}>
-        &copy; Copyright {new Date().getFullYear()} WordWise Inc.
+        &copy; Copyright {new Date().getFullYear()} MyWorldLog Inc.
       </p>
     </footer>
   );
